8dbc4d9c0ff578c1bda6140d2fe8433e17e06c09
cosmin-marginean <cos@hey.com>
2023-02-17 23:37:38 +0000

    Dokka wiring

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+git tag -l | xargs git tag -d
+git fetch --tags
+
+VERSION=`cat version.properties | grep "version" | awk -F' *= *' '{print $2}'`
+echo "Version is $VERSION"
+
+rm -rf docs/dokka
+./gradlew clean dokkaHtml
+./gradlew publish
+
+git add --all
+git commit -am "Release $VERSION"
+git push
+git tag "${VERSION}"
+git push --tags --force
+
+gh release create "${VERSION}" --verify-tag --title "kbods ${VERSION}" --notes "Version ${VERSION}"
+
+echo "Finished building version $VERSION"
+
